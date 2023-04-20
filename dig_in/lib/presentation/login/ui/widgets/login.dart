0953563f71a8bc95a2bf7802a2e98101193029f1
Estarly Amaya <estarly.amaya@servinformacion.com>
2023-04-19 22:55:58 -0500

    IMPLEMENTATION
    - Animate_do implemented

--- dig_in/lib/presentation/login/ui/widgets/login.dart
+++ dig_in/lib/presentation/login/ui/widgets/login.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:dig_in/presentation/global/widgets/global_widgets.dart';
 import 'package:dig_in/presentation/login/blocs/bloc/login_bloc.dart';
 import 'package:dig_in/presentation/login/ui/providers/login_widget_provider.dart';
@@ -12,63 +13,81 @@ class Login extends StatelessWidget {
     final state = LoginProvider.of(context).state;
     return Column(
       children: [
-        CustomCard(
-            widget: Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: size.width  * 0.05,
-                vertical  : size.height * 0.05
-              ),
-              child: Wrap(
-                children: [
-                  CustomTextField(
-                    textType: TextInputType.emailAddress, 
-                    controller: LoginProvider.of(context).controllerEmail, 
-                    hintText: "Email",
-                    error: 
-                      state.runtimeType == LoginState.error().runtimeType && 
-                        LoginProvider.of(context).controllerEmail.text.isEmpty?
-                          "Este campo debe estar lleno" : "",
-                    prefixIcon: Icon(Icons.email,color: Colors.grey.shade400,),
-                    textColor: Colors.grey,
-                    
+        FadeIn(
+          duration: Duration(milliseconds: 500),
+          child: SlideInUp(
+            duration: Duration(milliseconds: 500),
+            child: CustomCard(
+                widget: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: size.width  * 0.05,
+                    vertical  : size.height * 0.05
                   ),
-                  SizedBox(height: LoginProvider.of(context).size.height * 0.1,),
-                  CustomTextField(
-                    textType: TextInputType.emailAddress, 
-                    controller: LoginProvider.of(context).controllerPass, 
-                    hintText: "Password",
-                    textColor:Colors.grey,
-                    isPassword: true,
-                    error: 
-                      state.runtimeType == LoginState.error().runtimeType && 
-                        LoginProvider.of(context).controllerPass.text.isEmpty ?
-                          "Este campo debe estar lleno" : "",
-                    prefixIcon: Icon(Icons.lock_open_rounded,color: Colors.grey.shade400,),
-                  )
-
-                ],
-              ),
-            ),
-            shadowColor: Colors.grey,
-            elevation: 13,
-            backgroundColor: Colors.white,
-            cornerRadius: LoginProvider.of(context).size.width * 0.025),
-        SizedBox( height: size.height * 0.03,),
-        Align(
-          alignment: Alignment.centerRight,
-          child: CustomText(
-            text: "forgot password?",
-            textColor: Colors.blue.shade800,
-            fontSize: 17,
+                  child: Wrap(
+                    children: [
+                      CustomTextField(
+                        textType: TextInputType.emailAddress, 
+                        controller: LoginProvider.of(context).controllerEmail, 
+                        hintText: "Email",
+                        error: 
+                          state.runtimeType == LoginState.error().runtimeType && 
+                            LoginProvider.of(context).controllerEmail.text.isEmpty?
+                              "Este campo debe estar lleno" : "",
+                        prefixIcon: Icon(Icons.email,color: Colors.grey.shade400,),
+                        textColor: Colors.grey,
+                        
+                      ),
+                      SizedBox(height: LoginProvider.of(context).size.height * 0.1,),
+                      CustomTextField(
+                        textType: TextInputType.emailAddress, 
+                        controller: LoginProvider.of(context).controllerPass, 
+                        hintText: "Password",
+                        textColor:Colors.grey,
+                        isPassword: true,
+                        error: 
+                          state.runtimeType == LoginState.error().runtimeType && 
+                            LoginProvider.of(context).controllerPass.text.isEmpty ?
+                              "Este campo debe estar lleno" : "",
+                        prefixIcon: Icon(Icons.lock_open_rounded,color: Colors.grey.shade400,),
+                      )
+          
+                    ],
+                  ),
+                ),
+                shadowColor: Colors.grey,
+                elevation: 13,
+                backgroundColor: Colors.white,
+                cornerRadius: LoginProvider.of(context).size.width * 0.025),
           ),
         ),
+        SizedBox( height: size.height * 0.03,),
+        FadeIn(
+          duration: Duration(milliseconds: 700),
+          child: SlideInUp(
+            duration: Duration(milliseconds: 700),
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: CustomText(
+                text: "forgot password?",
+                textColor: Colors.blue.shade800,
+                fontSize: 17,
+              ),
+            ),
+          )
+        ),
         SizedBox( height:size.height * 0.05),
-        CustomButton(
-          color: Colors.orange.shade900,
-          onTap: () => LoginProvider.of(context).onTap(),
-          wait: LoginProvider.of(context).state.runtimeType == LoginState.loading().runtimeType,
-          text: "Login" ,
-          textColor: Colors.white,
+        FadeIn(
+          duration: Duration(milliseconds: 900),
+          child: SlideInUp(
+            duration: Duration(milliseconds: 900),
+            child: CustomButton(
+              color: Colors.orange.shade900,
+              onTap: () => LoginProvider.of(context).onTap(),
+              wait: LoginProvider.of(context).state.runtimeType == LoginState.loading().runtimeType,
+              text: "Login" ,
+              textColor: Colors.white,
+            ),
+          ),
         )
       ],
     );
--- dig_in/lib/presentation/login/ui/widgets/register.dart
+++ dig_in/lib/presentation/login/ui/widgets/register.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:dig_in/presentation/global/widgets/global_widgets.dart';
 import 'package:dig_in/presentation/login/ui/providers/register_widget_provider.dart';
 import 'package:flutter/material.dart';
@@ -12,60 +13,72 @@ class Register extends StatelessWidget {
     final registerProvider = RegisterProvider.of(context);
     return Column(
       children: [
-        CustomCard(
-          widget: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: registerProvider.size.width *0.05,
-              vertical: registerProvider.size.height * 0.05
+        FadeIn(
+          duration: Duration(milliseconds: 500),
+          child: SlideInUp(
+            duration: Duration(milliseconds: 500),
+            child: CustomCard(
+              widget: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: registerProvider.size.width *0.05,
+                  vertical: registerProvider.size.height * 0.05
+                ),
+                child: Wrap(
+                  children: [
+                    CustomTextField(
+                      textType: TextInputType.emailAddress, 
+                      controller: registerProvider.controllerName, 
+                      hintText: "Names", 
+                      textColor: Colors.grey,
+                      prefixIcon: Icon(Icons.person,color: Colors.grey.shade300,),
+                    ),
+                    SizedBox(height: registerProvider.size.height * 0.1,),
+                    CustomTextField(
+                      textType: TextInputType.emailAddress, 
+                      controller: registerProvider.controllerLastname, 
+                      hintText: "Last names", 
+                      textColor: Colors.grey,
+                      prefixIcon: Icon(Icons.person,color: Colors.grey.shade300,),
+                    ),
+                    SizedBox(height: registerProvider.size.height * 0.1,),
+                    CustomTextField(
+                      textType: TextInputType.emailAddress, 
+                      controller: registerProvider.controllerEmail, 
+                      hintText: "Email", 
+                      textColor: Colors.grey,
+                      prefixIcon: Icon(Icons.email,color: Colors.grey.shade300,),
+                    ),
+                    SizedBox(height: registerProvider.size.height * 0.1,),
+                    CustomTextField(
+                      textType: TextInputType.emailAddress, 
+                      controller: registerProvider.controllerPass, 
+                      hintText: "Password", 
+                      textColor: Colors.grey,
+                      prefixIcon: Icon(Icons.lock_open_outlined,color: Colors.grey.shade300,),
+                    ),
+                  ],
+                ),
+              ),
+              shadowColor: Colors.grey,
+              elevation: 13,
+              backgroundColor: Colors.white,
+              cornerRadius: registerProvider.size.width * 0.025,
             ),
-            child: Wrap(
-              children: [
-                CustomTextField(
-                  textType: TextInputType.emailAddress, 
-                  controller: registerProvider.controllerName, 
-                  hintText: "Names", 
-                  textColor: Colors.grey,
-                  prefixIcon: Icon(Icons.person,color: Colors.grey.shade300,),
-                ),
-                SizedBox(height: registerProvider.size.height * 0.1,),
-                CustomTextField(
-                  textType: TextInputType.emailAddress, 
-                  controller: registerProvider.controllerLastname, 
-                  hintText: "Last names", 
-                  textColor: Colors.grey,
-                  prefixIcon: Icon(Icons.person,color: Colors.grey.shade300,),
-                ),
-                SizedBox(height: registerProvider.size.height * 0.1,),
-                CustomTextField(
-                  textType: TextInputType.emailAddress, 
-                  controller: registerProvider.controllerEmail, 
-                  hintText: "Email", 
-                  textColor: Colors.grey,
-                  prefixIcon: Icon(Icons.email,color: Colors.grey.shade300,),
-                ),
-                SizedBox(height: registerProvider.size.height * 0.1,),
-                CustomTextField(
-                  textType: TextInputType.emailAddress, 
-                  controller: registerProvider.controllerPass, 
-                  hintText: "Password", 
-                  textColor: Colors.grey,
-                  prefixIcon: Icon(Icons.lock_open_outlined,color: Colors.grey.shade300,),
-                ),
-              ],
-            ),
-          ),
-          shadowColor: Colors.grey,
-          elevation: 13,
-          backgroundColor: Colors.white,
-          cornerRadius: registerProvider.size.width * 0.025,
+          )
         ),
         SizedBox( height:registerProvider.size.height * 0.1),
-        CustomButton(
-          color: Colors.orange.shade900,
-          onTap: () => registerProvider.onTap(),
-          wait: registerProvider.state.runtimeType == LoginState.loading().runtimeType,
-          text: "Login" ,
-          textColor: Colors.white,
+        FadeIn(
+          duration: Duration(milliseconds: 700),
+          child: SlideInUp(
+            duration: Duration(milliseconds: 700),
+            child: CustomButton(
+              color: Colors.orange.shade900,
+              onTap: () => registerProvider.onTap(),
+              wait: registerProvider.state.runtimeType == LoginState.loading().runtimeType,
+              text: "Login" ,
+              textColor: Colors.white,
+            ),
+          )
         )
       ],
     );
